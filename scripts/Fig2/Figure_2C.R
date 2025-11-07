5ce5e94ab4fdb2f5b691455544edd8a34d07015e
GerbenDuns <gerbenduns@live.com>
2025-11-07 14:14:14 -0800

    Create Figure_2C.R

---
+++ scripts/Fig2/Figure_2C.R
@@ -0,0 +1,50 @@
+## R code to create figure 2.C in Aoki et al, Multidimensional characterization of cellular ecosystems in Hodgkin lymphoma
+############## load packages ################ 
+library(tidyverse)
+library(ggplot2)
+library(ggsankey)
+############## define colors ################ 
+colors_HLGen <- c("CST" = "#A6FFB6",
+                  "CN913" = "#33A1F6",
+                  "STB" = "#D16303",
+                  "CN2P" = "#9370DB",
+                  "UNCLASS" = "#D3D3D3")
+
+colors_custom <- c(colors_HLGen,"OLD" = "#D3D3D3","YOUNG" = "cyan3","NEG" = "#D3D3D3","POS" = "#458B00")
+
+############## input data ################ 
+input_Figure_2C <- read_tsv("../../data/input_Figure_2C_Age_group_HLGen.tsv")
+#############
+df_sankey <- input_Figure_2C
+df_sankey_long <- df_sankey %>% 
+  make_long('Age group',HLGen)
+##
+df_sankey_long$node <- factor(df_sankey_long$node,levels= c("YOUNG","OLD","CN2P","STB","CN913","CST")) ## Change order
+
+df_sankey_long %>%
+  ggplot(aes(x = x
+             , next_x = next_x
+             , node = node
+             , next_node = next_node
+             , fill = factor(node)
+             , label = node)
+  ) +
+  geom_sankey(space = 0,
+              flow.alpha = 0.5
+              , node.color = "black"
+              ,show.legend = FALSE,
+              width = .4,
+  ) +
+  scale_x_discrete(position = "top") + # put axis (labels) on top of the graph +
+  theme(legend.position = "none",
+        axis.title = element_blank(),
+        axis.text.y = element_blank(),
+        axis.ticks = element_blank(),
+        axis.text.x = element_text(size = 18,face = "bold"),
+        panel.grid = element_blank(),
+        panel.background = element_blank())  +
+  scale_fill_manual(values = colors_custom_adj) +
+  geom_sankey_text(space = 0,size = 5,type = "sankey") +## space = 0 to match  
+  theme(axis.text.x.top = element_text(margin = margin(b = -20))) # Solution label position
+############## save plot ################
+ggsave("Figure2C_Age_Group_HLGen.pdf",height = 8, width = 6)
