cd20661d19b8a21cb53b80ee0e1685f2c10e12b9
GerbenDuns <gerbenduns@live.com>
2025-11-18 11:28:00 -0800

    Create Figures_6DEF.R

---
+++ scripts/Fig6/Figures_6DEF.R
@@ -0,0 +1,146 @@
+## R code to create figures 6D, 6E, and 6F in Aoki et al, Multidimensional characterization of cellular ecosystems in Hodgkin lymphoma
+########################################################################
+######################## Load packages ########################
+########################################################################
+library(readr)
+library(ggplot2)
+library(ggsankey)
+########################################################################
+######################## define HLGen colors ####################
+########################################################################
+colors_HLGen <- c("CST" = "#A6FFB6",
+                  "CN913" = "#33A1F6",
+                  "STB" = "#D16303",
+                  "CN2P" = "#9370DB",
+                  "UNCLASS" = "#D3D3D3")
+######################## read input data ########################     
+# Input_Main_Figure_6_DEF <- read_tsv("../../data/Fig6/Input_figures_6DEF.tsv")
+########################################################################
+######################## create alluvial/sankey plots ########################  
+########################################################################
+## GGSankey plot all samples: EBV
+#################################
+## Convert for sankey plots:
+df_sankey <- data.frame(
+                        Alig_Aixiang_preds_combined$EBV_consensus,
+                        Alig_Aixiang_preds_combined$HLGen) 
+colnames(df_sankey) <- c("EBV","HLGen")
+df_sankey$EBV[df_sankey$EBV == 0] <- "NEG"
+df_sankey$EBV[df_sankey$EBV == 1] <- "POS"
+##
+df_sankey_long <- df_sankey %>% 
+  make_long(EBV,HLGen)
+
+colors_custom <- c("NEG" = "#D3D3D3","POS" = "#458B00",colors_HLGen)
+df_sankey_long$node <- factor(df_sankey_long$node,levels= c("NEG","POS","UNCLASS","CN2P","STB","CN913","CST")) ## Change order
+df_sankey_long %>%
+  ggplot(aes(x = x
+                     , next_x = next_x
+                     , node = node
+                     , next_node = next_node
+                     , fill = factor(node)
+                     , label = node)
+             ) +
+  geom_sankey(space = 0,
+              flow.alpha = 0.5
+                      , node.color = "black"
+                      ,show.legend = FALSE,
+              width = .4,
+                ) +
+  scale_x_discrete(position = "top") + # put axis (labels) on top of the graph
+  theme(legend.position = "none",
+        axis.title = element_blank(),
+                axis.text.y = element_blank(),
+                 axis.ticks = element_blank(),
+         axis.text.x = element_text(size = 18,face = "bold",vjust =-2),
+        panel.grid = element_blank(),
+        panel.background = element_blank())  +
+  scale_fill_manual(values = colors_custom) +
+  geom_sankey_text(space = 0,size = 5,type = "sankey") ## space = 0 to match  
+
+ggsave("Figure_6D_sankey_HLGen_EBV_Alig.pdf",height = 8, width = 6)
+
+########################################################################
+## GGSankey plot all samples: Age
+#################################
+
+## Convert for sankey plots:
+df_sankey <- data.frame(
+                        Alig_Aixiang_preds_combined$AGE_group45,
+                        Alig_Aixiang_preds_combined$HLGen)
+colnames(df_sankey) <- c("Age group","HLGen")
+df_sankey$`Age group`[df_sankey$`Age group` == "young"] <- "YOUNG"
+df_sankey$`Age group`[df_sankey$`Age group` == "old"] <- "OLD"
+##
+df_sankey_long <- df_sankey %>% 
+  make_long(`Age group`,HLGen)
+
+colors_custom <- c("OLD" = "#D3D3D3","YOUNG" = "cyan3","NEG" = "#D3D3D3","POS" = "#458B00",colors_HLGen)
+df_sankey_long$node <- factor(df_sankey_long$node,levels= c("YOUNG","OLD","UNCLASS","CN2P","STB","CN913","CST")) ## Change order
+df_sankey_long %>%
+  ggplot(aes(x = x
+                     , next_x = next_x
+                     , node = node
+                     , next_node = next_node
+                     , fill = factor(node)
+                     , label = node)
+             ) +
+  geom_sankey(space = 0,
+              flow.alpha = 0.5
+                      , node.color = "black"
+                      ,show.legend = FALSE,
+              width = .4,
+                ) +
+  scale_x_discrete(position = "top") + # put axis (labels) on top of the graph
+  theme(legend.position = "none",
+        axis.title = element_blank(),
+                axis.text.y = element_blank(),
+                 axis.ticks = element_blank(),
+         axis.text.x = element_text(size = 18,face = "bold",vjust =-2),
+        panel.grid = element_blank(),
+        panel.background = element_blank())  +
+  scale_fill_manual(values = colors_custom) +
+  geom_sankey_text(space = 0,size = 5,type = "sankey") ## space = 0 to match  
+  
+ggsave("Figure_6E_sankey_HLGen_AGE_Alig.pdf",height = 8, width = 6)
+
+########################################################################
+## GGSankey plot all samples: Age
+#################################
+## Convert for sankey plots:
+df_sankey <- data.frame(Input_Main_Figure_6$LDA_cluster_Stanford,
+                        Input_Main_Figure_6$HLGen)
+colnames(df_sankey) <- c("LDA","HLGen")
+##
+df_sankey_long <- df_sankey %>% 
+  make_long(LDA,HLGen)
+
+colors_custom <- c("H1" = "red","H2" = "steelblue4",colors_HLGen)
+
+df_sankey_long$node <- factor(df_sankey_long$node,levels= c("H2","H1","UNCLASS","CN2P","STB","CN913","CST")) ## Change order
+df_sankey_long %>%
+  ggplot(aes(x = x
+                     , next_x = next_x
+                     , node = node
+                     , next_node = next_node
+                     , fill = factor(node)
+                     , label = node)
+             ) +
+  geom_sankey(space = 0,
+              flow.alpha = 0.5
+                      , node.color = "black"
+                      ,show.legend = FALSE,
+              width = .4,
+                ) +
+  scale_x_discrete(position = "top") + # put axis (labels) on top of the graph
+  theme(legend.position = "none",
+        axis.title = element_blank(),
+                axis.text.y = element_blank(),
+                 axis.ticks = element_blank(),
+         axis.text.x = element_text(size = 18,face = "bold",vjust =-2),
+        panel.grid = element_blank(),
+        panel.background = element_blank())  +
+  scale_fill_manual(values = colors_custom) +
+  geom_sankey_text(space = 0,size = 5,type = "sankey")  +## space = 0 to match  
+  theme(axis.text.x.top = element_text(margin = margin(b = -20))) # Solution label position
+ggsave("Figure_6F_sankey_HLGen_LDA_Alig.pdf",height = 8, width = 6)
